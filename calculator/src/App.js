74373bd1af1ef5acc0b78040779cd4de642d6a70
choisaelim <choisaelim@gmail.com>
2021-03-22 21:13:13 +0900

    +/- 로직 정리, 사칙연산 수정

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -11,26 +11,49 @@ const App = () => {
 
 	//11+3 > 11-3
 	//20*20 > 20*-20
-	const multiMinus = (item) => {
-		//마지막 숫자까지 자름 11+3 이면 11+까지
-		var temp = item.match(/\d+$/m)[0];
-		var res;
-		console.log(temp);
-		if (temp.substr(temp.length - 1, 1) === '.') {
-			//마지막 숫자 앞에 소숫점이 있으면 그 전 숫자까지 자름
-			// res = temp.match(/\d+$/m)[0];
+	const PositiveNegative = (num) => {
+		var res = '0';
+		if (!isNaN(num * 1)) {
+			res = (num * -1).toString();
 		} else {
-			//3+3이면 3-3으로, 3-3이면 3+3으로, 3*3이면 3*-3으로
-			switch (temp.substr(temp.length - 1, 1)) {
-				case '+':
-					res = temp.substr(temp.length - 2, 1) + '-' + item.match(/\d+$/m)[1];
+			var temp = num.match(/^.+[+\-*/%]/);
+			var test = '';
+			if (temp[0].length > 2 && !isNumber(temp[0].substr(temp[0].length - 2, 1))) {
+				debugger;
+				//20*3 에서 +/- 했다가 다시 +/- 하면 20*3이 되야 하는데 20*+3이 되는 부분 수정해야 함
+			}
+			switch (temp[0].substr(temp[0].length - 1, 1)) {
+				case '+': //마지막 숫자 앞의 부호가 + 일 때 ex 123+456 > 123-456
+					test = num.substr(temp[0].length, num.length - temp[0].length);
+					res = temp[0].substr(0, temp[0].length - 1) + '-' + test;
 					break;
-
+				case '-': //마지막 숫자 앞의 부호가 - 일 때 ex 123-456 > 123+456
+					test = num.substr(temp[0].length, num.length - temp[0].length);
+					res = temp[0].substr(0, temp[0].length - 1) + '+' + test;
+					break;
 				default:
+					// 123*456 > 123*-456
+					test = num.substr(temp[0].length, num.length - temp[0].length);
+					res = temp[0].substr(0, temp[0].length) + '-' + test;
 					break;
 			}
 		}
+
 		return res;
+	};
+
+	const isTwoOperator = (item) => {
+		if (item.length > 1) {
+			const operator = item.match(/[+\-*/%]?[+\-*/%]/);
+			if (operator[0].length === 2) {
+				//123*+46 > 123+46
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
 	};
 
 	const handleClick = useCallback((name) => {
@@ -38,40 +61,21 @@ const App = () => {
 		if (isNumber(name) || name === '.') {
 			setNumber((num) => (isNaN((num + name) * 1) ? num + name : ((num + name) * 1).toString()));
 		} else {
+			name = name.replace('÷', '/');
+			name = name.replace('x', '*');
 			switch (name) {
 				case '+/-':
-					setNumber((num) => {
-						var temp = num.match(/\d+$/);
-						var res = '0';
-						console.log(temp[1]);
-						if (temp[0].substr(temp[0].length - 1, 1) === '+') {
-						}
-						// switch (temp[0].substr(temp[0].length - 1, 1)) {
-						// 	case '+':
-						// 		console.log(temp[0].substr(0, temp[0].length - 1));
-						// 		res = temp[0].substr(0, temp[0].length - 1) + '-' + temp[1];
-						// 		break;
-						// 	default:
-						// 		break;
-						// }
-						return res;
-					});
+					setNumber((num) => PositiveNegative(num));
 					break;
 				case 'AC':
 					setNumber(0);
-					break;
-				case '÷':
-					setNumber((num) => num + '/');
-					break;
-				case 'x':
-					setNumber((num) => num + '*');
 					break;
 				case '=':
 					setNumber((num) => eval(num));
 					break;
 				default:
-					// + - % 경우 기호와 표시가 같음
-					setNumber((num) => num + name);
+					// + - % / 처리, Operator 뒤에 또 Operator 넣으면 제일 최근 Operator만 남김
+					setNumber((num) => (isTwoOperator(num + name) ? num.substr(0, num.length - 1) + name : num + name));
 					break;
 			}
 		}
